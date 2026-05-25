3365e63a1dd199198025d933258c72f85bce6cd7
Rahul S Kumar <rahulrockysunil@gmail.com>
2026-05-25 12:22:55 +0530

    Initial commit

---
+++ lib/font.ts
@@ -0,0 +1,30 @@
+import { Caveat, Inter, Poppins } from "next/font/google";
+import localFont from "next/font/local";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+  style: ["normal", "italic"],
+  display: "swap",
+});
+
+export const caveat = Caveat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-caveat",
+  style: ["normal"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-inter",
+  style: ["normal"],
+});
+
+export const galgin = localFont({
+  src: "../fonts/galgin.otf",
+  variable: "--font-galgin",
+  display: "swap",
+});
