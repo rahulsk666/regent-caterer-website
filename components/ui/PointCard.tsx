a8283fab37a2400e35908f8c2bdeb4deba0f6219
Rahul S Kumar <rahulrockysunil@gmail.com>
2026-06-03 20:32:53 +0530

    style: increase gap in WhyUsSection and adjust margins in PointCard for improved spacing

--- components/ui/PointCard.tsx
+++ components/ui/PointCard.tsx
@@ -4,12 +4,10 @@ export default function PointCard({ point }: PointCardProps) {
   return (
     <div className="w-full flex flex-col">
       {/* Title */}
-      <h3 className="text-center md:text-start text-3xl sm:text-4xl lg:text-5xl font-galgin font-medium text-golden-500 leading-tight">
+      <h3 className="text-center md:text-start text-3xl sm:text-4xl lg:text-5xl  font-galgin font-medium text-golden-500 leading-tight">
         {point.title}
       </h3>
-
-      <div className="h-px bg-golden-500 mb-4 md:mb-6" />
-
+      <div className="h-px bg-golden-500 mb-6 md:mb-8" />
       {/* Description */}
       <p className="text-base sm:text-lg lg:text-xl text-neutral-600 leading-relaxed max-w-md">
         {point.description}
