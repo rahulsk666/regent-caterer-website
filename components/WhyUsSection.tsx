a43a2a74ed87af464eb75d2192b390ebfef843a7
Rahul S Kumar <rahulrockysunil@gmail.com>
2026-06-03 18:31:53 +0530

    refactor: remove unnecessary client directives and improve PointCard layout styling

--- components/WhyUsSection.tsx
+++ components/WhyUsSection.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import React from "react";
 import PointCard from "./ui/PointCard";
 import { points } from "@/lib/data";
 
--- components/ui/PointCard.tsx
+++ components/ui/PointCard.tsx
@@ -1,16 +1,13 @@
-"use client";
-
 import { PointCardProps } from "@/lib/types";
 
 export default function PointCard({ point }: PointCardProps) {
   return (
-    <div>
+    <div className="w-full flex flex-col gap-2">
       {/* Title */}
-      <h3 className="text-3xl sm:text-4xl lg:text-5xl font-galgin font-medium text-golden-500 mb-4 md:mb-6 leading-tight">
+      <h3 className="text-center md:text-start text-3xl sm:text-4xl lg:text-5xl font-galgin font-medium text-golden-500 mb-4 md:mb-6 leading-tight">
         {point.title}
       </h3>
 
-      {/* Animated divider */}
       <div className="h-px bg-golden-500 mb-4 md:mb-6" />
 
       {/* Description */}
